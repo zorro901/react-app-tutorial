9430768c04944893f13880201b44a7d2227e8e24
zorro901 <zoro907@gmail.com>
2021-08-09 08:44:35 +0900

    74. ユーザー一覧画面の作成

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,32 @@
+import { Box, Image, Stack, Text } from "@chakra-ui/react";
+import { memo, ReactNode, VFC } from "react";
+
+type Props = {
+  imageUrl: string;
+  userName: string;
+  fullName: string;
+};
+
+export const UserCard: VFC<Props> = memo((props) => {
+  const { imageUrl, userName, fullName } = props;
+  return (
+    <Box w="260px" h="260px" bg="navy" borderRadius="10px" shadow="md" p={4}>
+      <Stack textAlign="center">
+        <Image
+          borderRadius="full"
+          boxSize="160px"
+          src={imageUrl}
+          alt={userName}
+          m="auto"
+          _hover={{ cursor: "pointer", opacity: 0.8 }}
+        />
+        <Text fontSize="lg" fontWeight="bold">
+          {userName}
+        </Text>
+        <Text fontSize="sm" color="gray">
+          {fullName}
+        </Text>
+      </Stack>
+    </Box>
+  );
+});
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,12 @@
 import { memo, VFC } from "react";
-
+import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
+import { UserCard } from "../organisms/user/UserCard";
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページ</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard imageUrl="" userName="aaa" fullName="bbbb" />
+      </WrapItem>
+    </Wrap>
+  );
 });
